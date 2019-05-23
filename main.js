8e70d12a0086b9ff5a49ae646a1cdcf999e712b5
saitgumus <saitgumus33@gmail.com>
2019-05-23 23:51:56 +0300

    v3 sync düzeltmeleri yapıldı

--- main.js
+++ main.js
@@ -4,11 +4,7 @@ const cors = require('cors');
 const bodyParser = require("body-parser");
 const dateFormat = require("dateformat");
 const sha256 = require("sha256");
-//const Request = require("request");
 const axios = require("axios");
-//const socket = require("socket.io");
-//const http = require("http");
-//const socketModule = require('./socket-server')
 
 var server = require('http').createServer(app)
 var io = require('socket.io').listen(server)
@@ -38,11 +34,16 @@ let db_urls = [
 //server veriables
 let nodes = [
   //"http://localhost:8080",
-  "https://node-test-238108.appspot.com",
- // "https://node-test2-238819.appspot.com"
+ // "https://node-test-238108.appspot.com",
+  "https://node-test2-238819.appspot.com"
 ];
 
 
+//zincirin en büyük index numarasını saklamak için kullanılacak..
+let LastBlockData = {
+  "index":"",
+  "hash":""
+}
 
 
 app.use(cors()); 
@@ -57,7 +58,7 @@ let Transaction = require("./blockchain").Transaction;
 let testChain = new BlockChain();
 
 //connection the db
- mongoose.connect(db_urls[2],{useNewUrlParser:true},err => {
+ mongoose.connect(db_urls[1],{useNewUrlParser:true},err => {
   if (err) throw err;
   else console.log("database connection successfully");
 });
@@ -67,68 +68,138 @@ let testChain = new BlockChain();
 
 
 
-//  SYNC functions- * -
 
-let syncUser = (user)=>{
-  sendMessageToSocket('user sync doing..')
-  nodes.forEach( (element)=>{
-    axios.post(element+'/addUser',user).then( (res)=>{
-      console.log(res.body);
-      sendMessageToSocket(res.body);
-    }).catch( (err)=>{
-      console.log(err);
-      sendMessageToSocket('there is some error for userSync :( ');
-    })
+
+//blockchain validations
+let isValidBlock = function(block){
+  return new Promise( (resolve,reject)=>{
+    if(block.hash == sha256(block.index.toString()+block.timeStamp+JSON.stringify(block.transactions)+block.previousHash).toString()){
+      console.log('blok doğrulandı')
+      resolve (true)
+    }else{
+      console.log('blok doğrulamadı')
+      // console.log(block.hash);
+      // console.log(sha256(block.index.toString()+block.timeStamp+JSON.stringify(block.transactions)+block.previousHash).toString())
+      reject (false)
+    }
   })
+    
 }
 
 
-let syncCar = (car)=>{
+//veritabanındaki son blok indexini alır
+let getLastIndexAndHash = ()=>{
+  return new Promise(
+    (res,rej)=>{
+      Block.find({},{'index':1,'hash':1}, (e,d)=>{
+      if(!e) res(d[0])
+      else rej(e)
+      }).sort({'index':-1}).limit(1);
+      
+    }
+  )
+  }
+
+  //diğer serverlardan blok bilgisini al..
+let reqLastBlockData = async()=>{
+  let data = LastBlockData;
+  for(item of nodes){
+    await axios.post(item+'/getlastblockdata').then(
+      (r=>{
+        if(data.index<r.data.index)
+            data = r.data
+      })
+    ).catch(
+      e=>{throw e}
+    )
+  }
+  return data;
+}
+  //lastblockdata'ya son kaydedilen block bilgilerini atar.. Bu fonk. yeni blok oluşturulduğu anda çalışır.
+let updateLastBlockData = async ()=>{
+  await getLastIndexAndHash().then(
+    r=>{
+      LastBlockData.index = r.index
+      LastBlockData.hash = r.hash
+    }
+  ).catch( e=> console.log(e));
+}
+
+
+
+//  SYNC functions- * -
+
+let syncUser = async (user)=>{
+    for(item of nodes){
+     await SendPost(item+'/addUser',user).then( (res)=>{
+       sendMessageToSocket(res)
+     }).catch( e=>{sendMessageToSocket(e)})
+    }
+}
+
+
+let syncCar = async (car)=>{
   sendMessageToSocket('sync.. for new car.');
-  nodes.forEach( (element)=>{
-    axios.post(element+'/addCar',car).then( (res)=>{
-      console.log(res.body.sonuc);
-      sendMessageToSocket(res.body)
-    }).catch( (err)=>{
-      console.log(err);
-    })
-  })
+  for(item of nodes){
+    await SendPost(item+'/addCar',car).then(
+      r=>{sendMessageToSocket(item+': '+r)}
+    )
+  }
 }
 
 
 //yeni oluşturulan bloğu diğer serverlara dağıtma..
-let syncBlock = (block)=>{
-  let message = {message:""};
+let syncBlock = async (block)=>{
   sendMessageToSocket('new block sync..');
-    nodes.forEach( (element)=>{
-      axios.post(element+'/newBlock',block)
-      .then( (res)=>{
-        console.log(res.data);
-
-          if(res.data[0].sonuc == "ok"){
-            console.log("blok "+element+" e eklendi.");
-            sendMessageToSocket('new block added to '+element);
-            message.message = "ok";
-          }else{
-            message.message = "not";
-          }
-        }
-      ).catch( (err)=>{
-        console.log(err);
-        sendMessageToSocket(err);
-      })
-    })
-    return message;
+for(item of nodes){
+  await SendPost(item+'/newBlock',block).then(
+    r=>{sendMessageToSocket(item+': '+r)}
+  )
+}
 }
 
 
 //istenilen index numaralı bloğu başka serverdan alma
-let getOneBlock = (index)=>{
-  axios.post( nodes[0]+'/oneBlock', {"index":index})
-  .then( (res)=>{
-    return res.body;
-  })
+let getOneBlock = async (index)=>{
+
+  for(item of nodes){
+    await axios.post(item+'/oneBlock', {"index":index})
+    .then( r=>{
+      if(r.data.index != 0) 
+          return r.data
+    }).catch( e=>{console.log(e)})
+  }
+  
+sendMessageToSocket('hatalı block diğer serverlardan alınamadı');
+  
+return new Block({
+  index: 0,
+  timeStamp:'',
+  transactions:[],
+  previousHash: '',
+  hash: ''
+});
 }
+
+
+//eksik olan bloğu tamamlamak için
+let eksikBlokTamamla = async (index)=>{
+   let block = getOneBlock(index)
+
+   if(block.index != 0){
+     block.save( (err)=>{
+       if(err) throw err;
+
+       sendMessageToSocket('eksik blok eklendi.')
+       console.log('eksik blok eklendi.')
+     })
+   }
+}
+
+
+
+
+
 
 
 
@@ -165,9 +236,12 @@ app.post("/newTransaction", (req, res) => {
    sendMessageToSocket('yeni islem alındı..');
 
   if(testChain.pendingTransactions.length >= 3){
+    updateLastBlockData();
+    new Promise( (resolve,reject)=>{  
+      //diğer serverlardan alınan son blok bilgisi..
+      let reqData = reqLastBlockData();
 
-    new Promise( (resolve,reject)=>{
-      
+
       Block.find({},(err,data)=>{
         if(err) throw err;
         else{
@@ -197,27 +271,30 @@ app.post("/newTransaction", (req, res) => {
             //doğrulama
             sendMessageToSocket('checking the chain...');
           for( i=1;i<data.length ; i++){
+           
+            //hashler kontrolü
             if(data[i].previousHash !== data[i-1].hash )
                   reject(data[i-1]);
-              else if(data[i].hash !== sha256(data[i].index.toString() + data[i].timeStamp + JSON.stringify(data[i].transactions) + data[i].previousHash).toString() )
+            
+            //içerik ve hash kontrolü
+            if(data[i].hash !== sha256(data[i].index.toString() + data[i].timeStamp + JSON.stringify(data[i].transactions) + data[i].previousHash).toString() )
               {
-                //console.log(data[i].hash);
-                //console.log(sha256(data[i].index.toString() + data[i].timeStamp + JSON.stringify(data[i].transactions) + data[i].previousHash).toString());
-
                 reject(data[i]);
               }
             //  console.log('test edildi: '+ data[i-1].index);
           }
           sendMessageToSocket('checked chain index:0 to index:'+data[i-1].index.toString());
-          resolve(data[i-1]);
+          resolve(reqData);
         }
       }).sort({index:1})
     })
     .then( (lastBlock)=>{
     
-  //askıdaki işlemleri temizle
+//zamanı al
   let time = dateFormat();
 
+
+//yeni blok oluştur
   let _block = new Block({
     index: lastBlock.index+1,
     timeStamp: time.toString(),
@@ -226,50 +303,44 @@ app.post("/newTransaction", (req, res) => {
     hash: sha256((lastBlock.index+1).toString() + time.toString() + JSON.stringify(testChain.pendingTransactions) +lastBlock.hash).toString()
   });
 
-  new Promise( (resolve,reject)=>{
-    let message = syncBlock(_block);
-
-    if(message.message == "ok"){
-      resolve(message.message)
-    }else{
-      reject('blok dağıtımı yapılamadı');
-    }
-
-   }).then( (res)=>{
-     if(res.index){//cevap olarak blok dönerse önce o bloğu db'ye ekle
-       let oldBlock = new Block(res);
-       oldBlock.save( (err)=>{
-         if(err) throw err;
-         console.log('serverdan gelen eski blok eklendi');
-       })
-     }
-   }).catch( (mesaj)=>{
-     console.log(mesaj);
-     sendMessageToSocket(mesaj);
-   })
-
   _block.save( (err)=>{
     if(err) throw err;
 
-
+//son blok bilgisini güncelle ve askıyı temizle
+    updateLastBlockData();
     testChain.pendingTransactions = [];
     console.log('yeni block veri tabanına eklendi. index: ' + _block.index);
     sendMessageToSocket('new block is added to mydb.. index:'+_block.index);
   });
 
+  //bloğu dağıt
+  syncBlock(_block);
+
     }).catch( (invalidBlock)=>{
-      console.log('hatalı block : ');
-      console.log(invalidBlock);
-      sendMessageToSocket('hatalı block ->index: '+invalidBlock.index);
+      //hatalı bloğu kaldır
+        Block.findOneAndDelete({index:invalidBlock.index},(err,res)=>{
+          if(err) throw err;
+
+          console.log('hatalı blog silindi');
+        })
+      
+
+      //hatalının yerine yenisini diğer serverlardan al
       let newBlock = new Block(getOneBlock(invalidBlock.index))
-      sendMessageToSocket('block onarılıyor');
-      newBlock.save( (err)=>{
-        if(err) throw err;
-        else{
-          console.log('blok düzeltildi');
-        sendMessageToSocket('onarılma tamamlandı.');
-        }
-      })
+      sendMessageToSocket(invalidBlock.index+' index numaralı block onarılıyor');
+
+      if(newBlock.index != 0){
+        newBlock.save( (err)=>{
+          if(err) throw err;
+          else{
+            console.log('blok düzeltildi');
+          sendMessageToSocket('onarılma tamamlandı.');
+          }
+        })
+      }else{
+        sendMessageToSocket('hatalı block onarılamadı..');
+      }
+      
     })
 
    }
@@ -292,7 +363,7 @@ app.post("/queryTransaction",(req,res)=>{
 
 
 
-//block sync
+//block sync için
 app.post("/getLastBlock",(req,res)=>{
   //cevap olarak son bloğu gönder
   Block.find({},(err,data)=>{
@@ -301,32 +372,19 @@ app.post("/getLastBlock",(req,res)=>{
   }).sort({index:-1}).limit(1);
 })
 
+
 //diğer bir serverdan yeni blok geldiğinde
 app.post("/newBlock",(req,res)=>{
-  //let newBlockIndex = req.body.index;
-  // let lastBlock;
-  //   Block.find({}, (err,data)=>{
-  //     if(err) throw err;
-      
-      
-  //   }).sort({index:-1}).limit(1);
-
-    // if(lastBlock.index < newBlockIndex){
-    //   let newBlock = new Block(req.body);
-    //   newBlock.save( (err)=>{
-    //     if(err) throw err;
-
-    //     res.statusCode(200).json({"sonuc":"yeni block eklendi"});
-    //   })
-    // }else if(lastBlock.index == newBlockIndex){
-    //   res.statusCode(405).json(lastBlock);
-    // }
+  
     let newBlock = new Block(req.body);
     newBlock.save( (err)=>{
-      if(err) throw err;
+      if(err){
+        res.json({sonuc:"basarısız"})
+        throw err;
+      }
 
-      res.status(200).json({sonuc:"ok"});
-      console.log('basarılı');
+      res.status(200).json({sonuc:"basarılı"});
+      console.log('yeni blok eklendi');
     })
 })
 
@@ -334,12 +392,16 @@ app.post("/newBlock",(req,res)=>{
 app.post("/oneBlock",(req,res)=>{
   Block.findOne({'index':req.body.index}, (err,data)=>{
     if (err) throw err;
-    else{
-      res.json(data);
-    }
+    isValidBlock(data).then( r=>{res.json(data)})
+    .catch( e=>{res.json({"e":0})})
   })
 })
 
+
+//son blok bilgilerini ver
+app.post("/getlastblockdata", (req,res)=>{
+  res.json(LastBlockData);
+})
 
 
 // yeni kullanıcı ekle ***
@@ -350,11 +412,12 @@ app.post("/newuser", (req, res) => {
     if (err) throw err;
 
     res.status(200).send(user);
-    sendMessageToSocket('yeni kullanıcı eklendi'+user.userName );
+    sendMessageToSocket('yeni kullanıcı eklendi: '+user.userName );
   })
  
   syncUser(user);
 });
+
 
 //diğer serverdan kullanıcı geldiğinde
 app.post("/addUser",(req,res)=>{
@@ -362,10 +425,11 @@ app.post("/addUser",(req,res)=>{
 
   user.save(function(err) {
     if (err) throw err;
-
-    res.status(200).JSON({"eklenen":user.userName});
+    sendMessageToSocket('added a user: '+ user.userName );
+    res.json({"sonuc":"basarılı"});
   })
 })
+
 //kullanıcı sorgulama
 app.post("/newuser/valid", (req,res)=>{
 
@@ -405,7 +469,7 @@ app.post("/addCar", (req,res)=>{
   _car.save(function(err) {
     if (err) throw err;
 
-    res.status(200).json({"sonuc":"başarılı"});
+    res.json({"sonuc":"başarılı"});
   });
 })
 
@@ -438,30 +502,48 @@ server.listen(8080);
 console.log('listening clients on port:8080');
 
 
+
 isConnect = false;
-
-
-
 
 
 io.sockets.on('connection', (socket)=>{
 
   isConnect = true;
+  
   socket.on('disconnect', function(data){
       isConnect = false;
   })
 
   socket.on('send message',function(data){
-//       io.sockets.emit('new message', { msg: data, test:'sait'})
+       io.sockets.emit('new message', { msg: data, test:'sait'})
   })
 })
 
 
-
-function sendMessageToSocket(data){
+function sendMessageToSocket(data='listening..'){
   if(isConnect){
       io.sockets.emit('test server', {msg:data});
   }else{
     console.log('socket connection error');
   }
 }
+
+
+
+
+
+
+//diğer serverlara post göndermek için..
+let SendPost = (url,data)=>{
+  return new Promise( (resolve,reject)=>{
+  axios.post(url,data).then( (res)=>{
+    if(res.data.sonuc)
+        resolve(res.data.sonuc)
+    else if(res.data.index)
+        resolve(res.data)
+  }).catch( (err)=>{
+    reject(err)
+  })
+  })
+}
+
